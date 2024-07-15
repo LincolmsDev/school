d07df41ecfbae8d792880f8dfe4eb476a7999c7d
Lincolms_dev <sebastien@lincolmsdev.fr>
2024-07-15 15:41:31 +0200

    init of projet php

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pannel de configuration</title>
+        <link rel="stylesheet" href="/assets/css/index.css">
+    </head>
+    <body class="body-f">
+        <main class="flex flex-col min-h-screen">
+            <header>
+             
+            </header>
+           <h1>test</h1>
+      
+        </main>
+        <footer>
+          
+        </footer>
+    </body>
+    <script src="https://kit.fontawesome.com/541a7507dd.js" crossorigin="anonymous"></script>
+   <script src="assets/js/script.js"></script>
+
+</html>
+
+<!--docker compose up -d-->
